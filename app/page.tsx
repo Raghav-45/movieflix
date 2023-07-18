3bedf66c1292e5a4089dc01f63fbccf6f88f1ed1
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-07-18 17:41:32 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import { Input } from '@/components/ui/input'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='flex h-14 w-full bg-red-500'>
+    <div className='flex h-14 w-full bg-black/60'>
       <div className='mr-auto'></div>
-      <div className='flex-auto max-w-[700px] bg-white items-center rounded-xl my-2'></div>
+      {/* <div className='flex-auto max-w-[700px] bg-white items-center rounded-xl my-2.5'> */}
+        <Input className='flex-auto h-auto max-w-[700px] bg-black/40 text-white !placeholder-white/40 items-center rounded-lg my-2.5 border-hidden focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0' placeholder='Search & run commands' />
+      {/* </div> */}
       <div className='ml-auto'></div>
     </div>
   )
