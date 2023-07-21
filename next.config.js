0edf0a606fe386c51ce568c6c297be9d3b626b9c
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-07-21 15:55:38 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        port: '',
+        // pathname: '/my-bucket/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.mykite.in',
+        port: '',
+        // pathname: '/my-bucket/**',
+      },
+    ],
+  },
+
+}
 
 module.exports = nextConfig
