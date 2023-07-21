bc4254e9abf856fb4a426984049fc03a71886028
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-07-21 15:55:41 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,39 @@
+import Navbar from '@/components/Navbar'
 import { Input } from '@/components/ui/input'
 import Image from 'next/image'
 
-export default function Home() {
+type Movie = {
+  id: number,
+  title: string,
+  original_title: string,
+  overview: string,
+  original_language: string,
+  poster_path: string,
+  backdrop_path: string,
+  release_date: string,
+}
+
+const getMovies = async (): Promise<Movie[]> => {
+  const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_API}&language=en-US&query=${'spiderman'}&page=1&include_adult=false`)
+  if (!res.ok) {
+    throw new Error('failed to fetch')
+  }
+  const data = await res.json()
+  return data.results
+}
+
+export default async function Home() {
+  const movies = await getMovies()
+  // console.log(movies.map(movie => movie.title))
   return (
     <div>
-      <div className='sticky top-0'>
-        <div className='flex h-14 w-full bg-[#666]'>
-          <div className='mr-auto order-first'></div>
-          {/* <div className='flex-auto max-w-[700px] bg-white items-center rounded-xl my-2.5'> */}
-            <Input className='flex-auto h-auto max-w-[700px] bg-black/40 text-white !placeholder-white/40 items-center rounded-lg m-2.5 border-hidden focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0' placeholder='Search & run commands' />
-          {/* </div> */}
-          <div className='ml-auto order-last'></div>
-        </div>
-      </div>
-
-      {/* <div className='flex'>
-        <img className='flex-none w-full aspect-[1/2]' src='https://www.w3schools.com/tags/img_girl.jpg'></img>
-      </div> */}
-      {/* <div className="grid grid-cols-2 gap-2.5 px-2.5 bg-gradient-to-b from-black/60 to-black/30"> */}
-      <div className="grid grid-cols-2 gap-2.5 px-2.5 bg-gradient-to-b from-black/60 from-50% via-black/60 via-80%">
-        <div className='flex-none w-full aspect-[27/41] rounded-xl overflow-hidden bg-blue-400'>
-          {/* <img className='h-full w-full' src='https://www.w3schools.com/tags/img_girl.jpg'></img> */}
-        </div>
-        <div className='flex-none w-full aspect-[27/41] rounded-xl overflow-hidden bg-blue-400'>
-          {/* <img className='h-full w-full' src='https://www.w3schools.com/tags/img_girl.jpg'></img> */}
-        </div>
-        <div className='flex-none w-full aspect-[27/41] rounded-xl overflow-hidden bg-blue-400'>
-          {/* <img className='h-full w-full' src='https://www.w3schools.com/tags/img_girl.jpg'></img> */}
-        </div>
-        <div className='flex-none w-full aspect-[27/41] rounded-xl overflow-hidden bg-blue-400'>
-          {/* <img className='h-full w-full' src='https://www.w3schools.com/tags/img_girl.jpg'></img> */}
-        </div>
-        <div className='flex-none w-full aspect-[27/41] rounded-xl overflow-hidden bg-blue-400'>
-          {/* <img className='h-full w-full' src='https://www.w3schools.com/tags/img_girl.jpg'></img> */}
-        </div>
-        <div className='flex-none w-full aspect-[27/41] rounded-xl overflow-hidden bg-blue-400'>
-          {/* <img className='h-full w-full' src='https://www.w3schools.com/tags/img_girl.jpg'></img> */}
-        </div>
-        <div className='flex-none w-full aspect-[27/41] rounded-xl overflow-hidden bg-blue-400'>
-          {/* <img className='h-full w-full' src='https://www.w3schools.com/tags/img_girl.jpg'></img> */}
-        </div>
-        <div className='flex-none w-full aspect-[27/41] rounded-xl overflow-hidden bg-blue-400'>
-          {/* <img className='h-full w-full' src='https://www.w3schools.com/tags/img_girl.jpg'></img> */}
-        </div>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-9 gap-2.5 px-2.5 bg-gradient-to-b from-black/60 from-50% via-black/60 via-80%">
+        {movies.map(movie => (
+          <div key={movie.id} className='relative flex-none w-full aspect-[27/41] rounded-xl overflow-hidden bg-blue-400'>
+            <Image fill className='h-full w-full' src={movie.poster_path ? `https://image.tmdb.org/t/p/original${movie.poster_path}` : 'https://www.mykite.in/kb/NoImageFound.jpg.png'} alt={movie.title} />
+            {/* <img className='h-full w-full' src={movie.poster_path ? `https://image.tmdb.org/t/p/original${movie.poster_path}` : 'https://www.mykite.in/kb/NoImageFound.jpg.png'} alt={movie.title} /> */}
+          </div>
+        ))}
         <div className='pb-[170px]'></div>
       </div>
     </div>
