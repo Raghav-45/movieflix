fca92ce881ed8e00ecfb32a0942e7c044dfbe28c
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-07-21 15:55:35 +0530

    Create Navbar.tsx

---
+++ components/Navbar.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react'
+import { Input } from './ui/input'
+
+interface NavbarProps {
+  
+}
+
+const Navbar: FC<NavbarProps> = ({}) => {
+  return (
+    <div className='sticky top-0'>
+      <div className='flex h-14 w-full bg-[#666]'>
+        <div className='mr-auto order-first'></div>
+          <Input className='flex-auto h-auto max-w-[700px] bg-black/40 text-white !placeholder-white/40 items-center rounded-lg m-2.5 border-hidden focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0' placeholder='Search & run commands' />
+        <div className='ml-auto order-last'></div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
