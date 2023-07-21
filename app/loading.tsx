445c0f143ed460b2dc36e71b9fcc4932402634a8
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-07-21 16:32:22 +0530

    Update loading.tsx

--- app/loading.tsx
+++ app/loading.tsx
@@ -5,7 +5,7 @@ export default function Loading() {
     <div>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-9 gap-2.5 px-2.5 bg-gradient-to-b from-black/60 from-50% via-black/60 via-80%">
         {Array.from({length: 20}, (_, i) => i + 1).map(movie => (
-          <div key={movie.i} className='relative flex-none w-full aspect-[27/41] rounded-xl overflow-hidden bg-white/5'>
+          <div key={movie} className='relative flex-none w-full aspect-[27/41] rounded-xl overflow-hidden bg-white/5'>
             <Skeleton className='h-full w-full bg-white/30'></Skeleton>
           </div>
         ))}
